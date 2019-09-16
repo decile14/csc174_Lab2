330cb9015302d20441933b3fd86597a08a1c6685
vacevedo44 <55210725+vacevedo44@users.noreply.github.com>
2019-09-16 19:13:42 -0400

    Add files via upload

---
+++ assignment02/inc/aacuna-intro.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Ana Acuña | Assignment 2</title>
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body class="container">
+	<header>
+		<a href="index.php">
+			<h1>Ana Sofía Acuña</h1>
+			<img src="images/me.jpg" alt="Picture of me">
+		</a>
+	</header>
+
+	<aside>
+		<ul>
+			<li>Marketing student flirting with Computer Science</li>
+			<li>Cat lover</li>
+			<li>Looking for the perfect first tattoo</li>
+		</ul>
+	</aside>
+
+	<nav>
+		<ul>
+			<li><a href="aacuna-main.php">Origins</a></li>
+			<li><a href="aacuna-journey.php">Journey</a></li>
+			<li><a href="aacuna-today.php">Today</a></li>
+			<li><a href="aacuna-tomorrow.php">Tomorrow</a></li>
+		</ul>
+	</nav>
---
+++ assignment02/jhuo3-earlydays.php
@@ -0,0 +1,17 @@
+<?php include "inc/jhuo3-intro.php"; ?>
+<?php include "inc/nav.php"; ?>
+
+	<main>
+		
+		<h1>Early Days</h1>
+		<p>My hometown is in Shanghai, China, but I have moved to San Francisco Bay Area since when I was thirteen years old. I studied at a Christian school in which I skipped my eighth grade and started from high school immediately. During my fantastic four years in high school, I have demonstrated strong interest in visual designs and various categories of art including calligraphy, painting, photography, and etc. With further curiosity, I have begun learning the technological instruments and software, particularly Adobe Photoshop and Blender.</p>
+		
+	</main>
+
+	<footer>CSC 174: Advanced Front-end Web Design</footer>
+
+	<script src="http://code.jquery.com/jquery.js"></script>
+	<script src="scripts/menu-highlighter.js"></script>
+
+</body>
+</html>
